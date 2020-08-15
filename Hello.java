ce8af99701d75e1c6e636409f85a5c2989ab42de
kirankumar <kirankumar050198@gmail.com>
2020-08-15 21:06:53 +0530

    second version of java code

--- Hello.java
+++ Hello.java
@@ -1,11 +1,14 @@
 package com.app;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
-			
-		System.out.println("Hello Kiran");
-
+		Scanner sc = new Scanner(System.in);	
+		System.out.println("Please enter your name: ");
+		String name = sc.next();
+		System.out.println("Hello "+name);
 	}
 
 }
